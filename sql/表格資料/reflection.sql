7337991e5293ca0eee4808966ef6d37262768742
11044127 <ggyytiffany2127@gmail.com>
2023-06-08 21:19:57 +0800

    no message

---
+++ sql/表格資料/reflection.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: product_search
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reflection`
+--
+
+DROP TABLE IF EXISTS `reflection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reflection` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `photo_url` varchar(45) DEFAULT NULL,
+  `text_content` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO reflection (photo_url, text_content) 
+VALUES 
+("https://fakeimg.pl/400x300/", "在这本书中，作者以独特的视角和精彩的叙述带领读者进入了一个全新的世界。故事情节扣人心弦，让人无法放下，每一页都充满了惊喜和紧张。主人公的形象栩栩如生，他们的内心世界和情感变化让人产生共鸣。书中所描绘的场景细腻而生动，读者仿佛置身其中，感受到了作者的用心与创造力。另外，作者还融入了深刻的思考和哲学意味，使整个故事更加富有内涵和启发。这本书引人入胜，不仅是一部令人愉快的娱乐读物，更是一部值得反复品味和思考的文学作品。读完这本书，我对作者的才华和创作能力充满了敬佩之情。我强烈推荐这本书给所有喜爱阅读的人，相信它会给你带来难以忘怀的阅读体验。"),
+("https://fakeimg.pl/400x300/", "在这本书中，作者以独特的视角和精彩的叙述带领读者进入了一个全新的世界。故事情节扣人心弦，让人无法放下，每一页都充满了惊喜和紧张。主人公的形象栩栩如生，他们的内心世界和情感变化让人产生共鸣。书中所描绘的场景细腻而生动，读者仿佛置身其中，感受到了作者的用心与创造力。另外，作者还融入了深刻的思考和哲学意味，使整个故事更加富有内涵和启发。这本书引人入胜，不仅是一部令人愉快的娱乐读物，更是一部值得反复品味和思考的文学作品。读完这本书，我对作者的才华和创作能力充满了敬佩之情。我强烈推荐这本书给所有喜爱阅读的人，相信它会给你带来难以忘怀的阅读体验。"),
+("https://fakeimg.pl/400x300/", "在这本书中，作者以独特的视角和精彩的叙述带领读者进入了一个全新的世界。故事情节扣人心弦，让人无法放下，每一页都充满了惊喜和紧张。主人公的形象栩栩如生，他们的内心世界和情感变化让人产生共鸣。书中所描绘的场景细腻而生动，读者仿佛置身其中，感受到了作者的用心与创造力。另外，作者还融入了深刻的思考和哲学意味，使整个故事更加富有内涵和启发。这本书引人入胜，不仅是一部令人愉快的娱乐读物，更是一部值得反复品味和思考的文学作品。读完这本书，我对作者的才华和创作能力充满了敬佩之情。我强烈推荐这本书给所有喜爱阅读的人，相信它会给你带来难以忘怀的阅读体验。");
+
+--
+-- Dumping data for table `reflection`
+--
+
+LOCK TABLES `reflection` WRITE;
+/*!40000 ALTER TABLE `reflection` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reflection` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-08 20:44:17
